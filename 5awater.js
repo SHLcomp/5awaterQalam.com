afbc43182bcec79b9085ff7d32c811da852c1044
Sarah <91339435+SHLcomp@users.noreply.github.com>
2022-02-23 15:59:15 +0300

    Update 5awater.js

--- 5awater.js
+++ 5awater.js
@@ -1,5 +1,6 @@
 const burg = document.querySelector(".burg");
 const navMenu = document.querySelector(".nav-menu");
+const mobMenu = document.querySelector(".menu-barmobile")
 
 burg.addEventListener("click", () => {
     burg.classList.toggle("active");
@@ -9,4 +10,16 @@ burg.addEventListener("click", () => {
 document.querySelectorAll(".a").forEach(n => n. addEventListener("click" , () => {
         burg.classList.remove("active");
         navMenu.classList.remove("active");
+        mobMenu.classList.remove("active");
     }))
+
+
+    burg.addEventListener("click", () => {
+        mobMenu.classList.toggle("active");
+    })
+
+    document.querySelectorAll(".am , .a-all-divs , .all-divs").forEach(n => n. addEventListener("click" , () => {
+        mobMenu.classList.remove("active");
+        burg.classList.remove("active");
+    }))
+
